6e0b8407ac8f68f33e923b60bec1d7da363c2e71
Adam Dean <mikkydean@googlemail.com>
2024-05-07 10:33:34 +0100

    Completed task 5 - View individual article

---
+++ src/components/Article.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { getArticleById } from "../api"
+
+function Article () {
+    const [article, setArticle] = useState({})
+    const { article_id } = useParams()
+    const [isLoading, setIsLoading] = useState(true)
+    
+    useEffect(() => {
+        getArticleById(article_id).then((response) => {
+            setArticle(response.data.article)
+            setIsLoading(false)
+        })
+    }, [])
+
+if(isLoading) {
+    return <h2>Loading...</h2>
+}
+
+    return <article>
+        <h2>{article.title}</h2>
+        <img src={article.article_img_url}/>
+        <p>Topic: {article.topic}</p>
+        <p>Votes: {article.votes}</p>
+        <p>Author: {article.author}</p>
+        <p>Date: {article.created_at}</p>
+        <p>{article.body}</p>
+    </article>
+
+}
+
+export default Article
--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom"
+
 function ArticleCard ({article}) {
 
     const capitalisedTopic = article.topic.charAt(0).toUpperCase() + article.topic.slice(1)
 
     return <li>
+        <Link to={`/articles/${article.article_id}`}>
         <h3>{article.title}</h3>
         <img src={article.article_img_url} width="250px"/>
         <p>{capitalisedTopic} Votes: {article.votes}</p>
+        </Link>
     </li>
 
 }
